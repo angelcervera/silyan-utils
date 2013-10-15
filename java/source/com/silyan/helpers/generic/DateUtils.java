1733259a3b63629d683a0aa22f2b66344f06084a
Ángel Cervera Claudio <angelcervera@silyan.com>
2013-10-15 12:49:37 +0200

    DateUtils

---
+++ java/source/com/silyan/helpers/generic/DateUtils.java
@@ -0,0 +1,85 @@
+/**
+ * 
+ */
+package com.silyan.helpers.generic;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
+
+/**
+ * @author Angel Cervera Claudio ( angelcervera@silyan.com )
+ *
+ */
+public class DateUtils {
+	
+	/**
+	 * Number of days between two dates.
+	 * 
+	 * @param from
+	 * @param to
+	 * @return
+	 */
+	public static long daysDiff(Date from, Date to) {
+		return Math.round( (to.getTime() - from.getTime()) / 86400000D ); // 1000 * 60 * 60 * 24
+	}
+	
+	/**
+	 * Return UTC system's date.
+	 * 
+	 * @return
+	 */
+	public static Date getNow() {
+		return GregorianCalendar.getInstance(TimeZone.getTimeZone("UTC")).getTime();
+	}
+	
+	private final static String datePatter = "yyyy-MM-dd";
+	private final static String dateTimePatter = "yyyy-MM-dd HH:mm:ss.SSS";
+	private final static String timeZoneCode = "UTC";
+	
+	
+	public static SimpleDateFormat buildSimpleDateFormat() {
+		SimpleDateFormat sdf = new SimpleDateFormat(datePatter);
+		sdf.setTimeZone(TimeZone.getTimeZone(timeZoneCode));
+		sdf.setLenient(false);
+		return sdf;
+	}
+	
+	public static SimpleDateFormat buildSimpleDateTimeFormat() {
+		SimpleDateFormat sdf = new SimpleDateFormat(dateTimePatter);
+		sdf.setTimeZone(TimeZone.getTimeZone(timeZoneCode));
+		sdf.setLenient(false);
+		return sdf;
+	}
+	
+	public static Date parseDate(String strDate) throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat(datePatter);
+		sdf.setTimeZone(TimeZone.getTimeZone(timeZoneCode));
+		sdf.setLenient(false);
+		return sdf.parse(strDate);
+	}
+
+	public static String formatDate(Date date) throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat(datePatter);
+		sdf.setTimeZone(TimeZone.getTimeZone(timeZoneCode));
+		sdf.setLenient(false);
+		return sdf.format(date);
+	}
+	
+	public static Date parseDateTime(String strDate) throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat(dateTimePatter);
+		sdf.setTimeZone(TimeZone.getTimeZone(timeZoneCode));
+		sdf.setLenient(false);
+		return sdf.parse(strDate);
+	}
+
+	public static String formatDateTime(Date date) throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat(dateTimePatter);
+		sdf.setTimeZone(TimeZone.getTimeZone(timeZoneCode));
+		sdf.setLenient(false);
+		return sdf.format(date);
+	}
+
+}
---
+++ java/test/com/silyan/helpers/generic/DateUtilsTest.java
@@ -0,0 +1,33 @@
+package com.silyan.helpers.generic;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class DateUtilsTest {
+	
+	SimpleDateFormat sdf;
+	
+	@BeforeClass
+	public void init() {
+		 sdf = new SimpleDateFormat("yyyy-MM-dd");
+		 sdf.setLenient(false);
+	}
+	
+	@Test
+	public void daysDiff() throws ParseException {
+		Assert.assertEquals( DateUtils.daysDiff(sdf.parse("2012-12-31"), sdf.parse("2013-1-1")), 1L);
+		Assert.assertEquals( DateUtils.daysDiff(sdf.parse("2013-2-1"), sdf.parse("2013-2-28")), 27L);
+		Assert.assertEquals( DateUtils.daysDiff(sdf.parse("2013-2-1"), sdf.parse("2013-3-1")), 28L);
+		Assert.assertEquals( DateUtils.daysDiff(sdf.parse("2016-2-27"), sdf.parse("2016-2-29")), 2L); // bisiesto
+		Assert.assertEquals( DateUtils.daysDiff(sdf.parse("2016-2-27"), sdf.parse("2016-3-1")), 3L); // bisiesto
+		Assert.assertEquals( DateUtils.daysDiff(sdf.parse("2013-2-27"), sdf.parse("2013-3-1")), 2L); // no bisiesto
+		Assert.assertEquals( DateUtils.daysDiff(sdf.parse("2016-1-1"), sdf.parse("2017-1-1")), 366L); // bisiesto
+		Assert.assertEquals( DateUtils.daysDiff(sdf.parse("2013-1-1"), sdf.parse("2014-1-1")), 365L); // no bisiesto
+		Assert.assertEquals( DateUtils.daysDiff(sdf.parse("2013-3-30"), sdf.parse("2013-4-3")), 3L); // spanish change hour.
+		Assert.assertEquals( DateUtils.daysDiff(sdf.parse("2013-10-26"), sdf.parse("2013-10-30")), 4L); // spanish change hour.
+	}
+}
